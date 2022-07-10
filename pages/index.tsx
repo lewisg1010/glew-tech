f35bd2b23fd3b53ccaf88036704823b724f0625c
lewisg1010 <70862259+lewisg1010@users.noreply.github.com>
2022-07-10 00:22:13 -0400

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import Navbar from '../src/NavBar'
+import Navbar from '../src/Navbar'
 import HeroSection from '../src/HeroSection'
 
 
